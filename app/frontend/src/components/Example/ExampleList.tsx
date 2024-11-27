d887b3a3289f8b965f36ad1739738c66b6cc26cd
Kyriakos Antoniadis <contact@ai-systems.today>
2024-11-27 19:05:57 +0000

    css variation for disclaimer

--- app/frontend/src/components/Example/ExampleList.tsx
+++ app/frontend/src/components/Example/ExampleList.tsx
@@ -43,10 +43,7 @@ export const ExampleList = ({ onExampleClicked, useGPT4V }: Props) => {
             
             {/* Add Disclaimer Here */}
             <p className={styles.disclaimer}>
-                Disclaimer: This information has been obtained from TIF International Guidelines, which were developed 
-                by renowned international experts in the field of haemoglobinopathies. 
-                <a href="https://thalassaemia.org.cy/haemoglobin-disorders/clinical-trial-updates/" target="_blank" rel="noopener noreferrer"> Please visit our website</a> 
-                for bi-monthly updates on new advances and clinical trials.
+                <b>Disclaimer:</b>This information has been obtained from TIF International Guidelines, which were developed by renowned international experts in the field of haemoglobinopathies.  Please visit our <a href="https://thalassaemia.org.cy/haemoglobin-disorders/clinical-trial-updates/" target="_blank" rel="noopener noreferrer"> website</a> for bi-monthly updates on new advances and clinical trials.
             </p>
         </div>
     );
